8bb4adfc3c92e24f2e795abd4b64513eece2ee9e
mileke <mac@milekes-m3.local>
2026-01-07 09:01:30 +0100

    code commit

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(s string) string {
+	return "When the pupput grouws up, it says: " + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mileke/dog
+
+go 1.25.5
